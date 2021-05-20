d23de55b7756b0d8337dcc30931f2f1e93cf3d62
Daniel Lara <danielwaismannlara@gmail.com>
2021-05-20 12:02:28 -0300

    botao investimento diferente

--- routes/investments.js
+++ routes/investments.js
@@ -2,9 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/addInvestimentos', function(req, res, next) {
+router.get('/addInvestiment', function(req, res, next) {
   res.render('users/user/meusInvestimentos/addInvestment')
 
 })
 
+router.get('/listInvestment', function(req, res, next) {
+  res.render('users/user/meusInvestimentos/listInvestment')
+
+})
 module.exports = router;
