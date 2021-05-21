adac198d8be7c49c57f99a4caed075aaff27ee64
Daniel Lara <danielwaismannlara@gmail.com>
2021-05-21 11:07:45 -0300

    formulario meus investimentos

--- routes/investments.js
+++ routes/investments.js
@@ -1,14 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const array = [];
 
 /* GET users listing. */
 router.get('/addInvestiment', function(req, res, next) {
   res.render('users/user/meusInvestimentos/addInvestment')
 
-})
+});
+router.post('/addInvestment', (req,res) => {
+  console.log(req.body)
+  array.push(req.body)
+  res.redirect('/user/meusInvestimentos/listInvestment')
+});
 
-router.get('/listInvestment', function(req, res, next) {
-  res.render('users/user/meusInvestimentos/listInvestment')
-
-})
+router.get('/listInvestment', (req,res) =>{
+    res.render('users/user/meusInvestimentos/listInvestment', {investimentos: array})
+});
 module.exports = router;
