9c517e85a926e607c4c8a9810f838710cbd17949
Daniel Lara <danielwaismannlara@gmail.com>
2021-06-05 13:57:01 -0300

    botão de deletar

--- routes/investments.js
+++ routes/investments.js
@@ -9,11 +9,33 @@ router.get('/addInvestiment', function(req, res, next) {
 
 });
 router.post('/addInvestment', (req,res) => {
-  array.push(req.body)
+  const investmentNovo = {
+    id:array.length + 1,
+    ...req.body
+
+  }
+  array.push(investmentNovo)
   res.redirect('/user/meusInvestimentos/listInvestment')
 });
 
 router.get('/listInvestment', (req,res) =>{
     res.render('users/user/meusInvestimentos/listInvestment', {investimentos: array})
 });
+
+router.post('/deletar/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+
+  for (let i = array.length - 1; i >= 0; i--) {
+      const investimento = array[i]
+
+      if (investimento.id === id) {
+          array.splice(i, 1)
+          break
+      }
+  }
+
+  res.redirect('/user/meusInvestimentos/listInvestment')
+})
+
+
 module.exports = router;
--- routes/wallet.js
+++ routes/wallet.js
@@ -1,25 +1,64 @@
 const express = require('express');
 const router = express.Router();
 
-const arrayGastos = [];
-const arrayDespesas =[];
+const arrayEntrada = [];
+const arraySaida =[];
 
 /* GET users listing. */
 router.get('/addCarteira', function(req, res, next) {
   res.render('users/user/minhaCarteira/addCarteira')
 
 });
-router.post('/addCarteira', (req,res) => {
-  arrayGastos.push(req.body)
+router.post('/addCarteiraEntrada', (req,res) => {
+  const entradaNova = {
+    id:arrayEntrada.length +1,
+    ...req.body
+  }
+  arrayEntrada.push(entradaNova)
+  res.redirect('/user/minhaCarteira/listCarteira')  
+});
+
+router.post('/addCarteiraSaida', (req,res) => {
+  const saidaNova = {
+    id:arraySaida.length +1,
+    ...req.body
+  }
+  arraySaida.push(saidaNova)
   res.redirect('/user/minhaCarteira/listCarteira')
-  console.log(arrayGastos)
 });
 
 router.get('/listCarteira', (req,res) =>{
-    res.render('users/user/minhaCarteira/listCarteira', {
-      gastos: arrayGastos
-     
-    })
+  res.render('users/user/minhaCarteira/listCarteira', {entradas: arrayEntrada, saidas: arraySaida})
 });
+
+router.post('/deletarEntrada/:id', (req, res) => {
+  const idEntrada = parseInt(req.params.id)
+
+  for (let i = arrayEntrada.length - 1; i >= 0; i--) {
+      const entrada = arrayEntrada[i]
+
+      if (entrada.id === idEntrada) {
+          arrayEntrada.splice(i, 1)
+          break
+      }
+  }
+
+  res.redirect('/user/minhaCarteira/listCarteira')
+})
+
+router.post('/deletarSaida/:id', (req, res) => {
+  const idSaida = parseInt(req.params.id)
+
+  for (let i = arraySaida.length - 1; i >= 0; i--) {
+      const saida = arraySaida[i]
+
+      if (saida.id === idSaida) {
+          arraySaida.splice(i, 1)
+          break
+      }
+  }
+
+  res.redirect('/user/minhaCarteira/listCarteira')
+})
 
 module.exports = router;
