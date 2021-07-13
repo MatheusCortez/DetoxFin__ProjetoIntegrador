a096721f8077a979e7b0ca191966a11929371e4b
Matheus Cortez <matheus.cortez@live.com>
2021-07-13 17:18:30 -0300

    m

--- controllers/crypFunctions/regex.js
+++
@@ -1,5 +0,0 @@
- module.exports.Formatarcpf= function(cpf){
-   const cpfLimpo= cpf.replace(/\D/g,'');
-    const cpfFormatado = cpfLimpo.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g,'xxx$2$3xx')
-    return cpfFormatado
- }
