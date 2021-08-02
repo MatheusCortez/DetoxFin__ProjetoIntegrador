6155efe59071381c7e4821467010f3ca7bcf112f
Matheus Cortez <matheus.cortez@live.com>
2021-08-02 11:06:25 -0300

    feature:grafico minha carteira

--- controllers/menuLateral.controller.js
+++ controllers/menuLateral.controller.js
@@ -1,7 +1,37 @@
-module.exports.showMinhaCarteira =function(req,res,next){
+const models = require('../database/models')
+
+const Grafico = require('../public/js/grafico/grafico')
+
+module.exports.showMinhaCarteira = async function(req,res,next){
   const usuario = req.session.usuario
+
+
+    const user = await models.carteira.findOne({
+        where: {
+            Usuario_idUsuario: usuario.idUsuario
+        }
+    })
+    const carteira = await models.ganhogastos.findAll({
+        where: {
+            Carteira_Usuario_idUsuario: usuario.idUsuario,
+            Carteira_idCarteira: user.idCarteira
+        },
+       
+    })
+    const descricao=[];
+    const valor=[];
+
+    for(let i=0;i<carteira.length;i++){
+        descricao.push(carteira[i].dataValues.descricao)
+        valor.push(carteira[i].dataValues.valor)
+    }
+    Grafico(descricao,valor)//esta dando como nao é uma funcao ¬¬
+    console.log(descricao,valor)
+    console.log(Grafico)//type funcao anonima '---'
+    
   
     res.render('pages/internas/index',{usuario})
+
 
     
   }
